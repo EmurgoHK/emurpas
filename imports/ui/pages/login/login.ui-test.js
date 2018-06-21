889eb170908250a83d0c6c57f541887d7206f22f
Samuel Ralak <samuelralak@hotmail.com>
2018-06-21 19:13:41 +0300

    Problem: auth isn't tested #20
    Solution: added client side tests for auth pages i.e sign in and sign up. added client side tests for the header

---
+++ imports/ui/pages/login/login.ui-test.js
@@ -0,0 +1,42 @@
+const assert = require('assert')
+const baseUrl = 'http://localhost:3000/login'
+
+describe('Sign in page', () => {
+    it ('should have a \`Login\` header and subtitle', () => {
+        browser.url(baseUrl)
+        assert(browser.isExisting('h1=Login'), true)
+        assert(browser.isVisible('h1=Login'), true)
+
+        assert(browser.isExisting('p=Sign In to your account'), true)
+        assert(browser.isVisible('p=Sign In to your account'), true)
+    })
+
+    it ('should redirect to login when user has an account', () => {
+        browser.url(baseUrl)
+        assert(browser.isExisting('#goToSignup'), true)
+        assert(browser.isVisible('#goToSignup'), true)
+
+        browser.click('#goToSignup')
+        assert.equal(browser.getUrl(), 'http://localhost:3000/signup')
+    })
+
+    it ('should login user and redirect to home', () => {
+        browser.url(baseUrl)
+        browser.execute(() => {
+            Meteor.call('generateTestUser', (err, data) => {})
+            return 'ok'
+        })
+
+        browser.pause(3000)
+
+        browser.setValue('#email', 'derp@test.com')
+        browser.setValue('#password', '1234')
+        browser.click('button[type=submit]')
+        
+        browser.pause(3000)
+
+        assert(browser.isExisting('#signOut'), true)
+        assert(browser.isVisible('#signOut'), true)
+        assert.equal(browser.getUrl(), 'http://localhost:3000/')
+    })
+})
