7ff58048cd3adccad90be266a8234a821d9d0794
Samuel Ralak <samuelralak@hotmail.com>
2018-07-29 08:26:14 +0300

    problem: logged out users can see forms #29
    solution: moved applications questions to the newApplication route and added route filter preventing logged out users from accessing form questions

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,15 +1,17 @@
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import { BlazeLayout } from 'meteor/kadira:blaze-layout'
+import { notify } from "/imports/modules/notifier"
 
 // Import needed templates
 import '/imports/ui/pages/home/home'
 import '/imports/ui/pages/userInfo/userInfo'
+import '/imports/ui/pages/applications/new'
 import '/imports/ui/pages/login/login'
 import '/imports/ui/pages/signup/signup'
 import '/imports/ui/pages/example/example'
 import '/imports/ui/pages/not-found/not-found'
 
-const userLoginFilter = (context, redirect, stop) => {
+const userLoginFilter = (context, redirect, _stop) => {
 	let oldRoute = '/'
 	let authRoutes = ['/login', '/signup']
 
@@ -21,9 +23,19 @@ const userLoginFilter = (context, redirect, stop) => {
 	if (Meteor.userId() && authRoutes.includes(context.path)) {
 		redirect(oldRoute)
 	}
+
+	if (!Meteor.userId() && !authRoutes.includes(context.path)) {
+		notify("Login to continue!", "error")
+		redirect('/login')
+	}
 }
 
-FlowRouter.triggers.enter([userLoginFilter], { except: [] })
+// Redirect to login
+Accounts.onLogout((user) => {
+	FlowRouter.go('/login')
+})
+
+FlowRouter.triggers.enter([userLoginFilter], { except: ['App.home'] })
 
 // Set up all routes in the app
 FlowRouter.route('/', {
@@ -46,6 +58,17 @@ FlowRouter.route('/userInfo', {
     		main: 'userInfo'
     	})
   	}
+})
+
+FlowRouter.route('/applications/new', {
+	name: 'newApplication',
+	action: () => {
+	  BlazeLayout.render('App_body', {
+		  header: 'header',
+		  sidebar: 'sidebar',
+		  main: 'newApplication'
+	  })
+	}
 })
 
 FlowRouter.route('/login', {
