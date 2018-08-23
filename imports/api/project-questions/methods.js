0acce4f3001be2b65e27ae493e3fc52a066b2749
Samuel Ralak <samuelralak@hotmail.com>
2018-08-23 15:20:38 +0300

    problem: user-info form too large #36
    solution: used meteor to break down the userQuestions form. updated tests

--- imports/api/project-questions/methods.js
+++ imports/api/project-questions/methods.js
@@ -2,7 +2,7 @@ import { ValidatedMethod } from 'meteor/mdg:validated-method'
 import SimpleSchema from 'simpl-schema'
 
 import { ProjectQuestions } from './project-questions'
-import { FormProgress } from '../form-progress/form-progress'
+import { updateFormProgress } from '../form-progress/methods'
 
 SimpleSchema.extendOptions(['autoform'])
 
@@ -13,28 +13,12 @@ export const saveProjectQuestions = new ValidatedMethod({
         if (Meteor.isServer) {
             if (Meteor.userId()) {
                 if (projectID === 'new') {
-                    // insert questions data and get id
                     projectID = ProjectQuestions.insert(data, {validate: false})   
-
-                    // track progress for new data
-                    FormProgress.insert({
-                        user_id: Meteor.userId(),
-                        form_type: 'project',
-                        form_type_id: projectID,
-                        last_step: steps.last,
-                        next_step: steps.next,
-                        created_at: new Date().getTime(),
-                        updated_at: new Date().getTime()
-                    })
                 } else {
                     ProjectQuestions.update({ '_id' : projectID }, { $set : data })
-                    FormProgress.update({ 'user_id' : Meteor.userId(), 'form_type_id' : projectID }, { $set : {
-                        last_step: steps.current,
-                        next_step: steps.next,
-                        updated_at: new Date().getTime()
-                    }})
                 }
 
+                updateFormProgress('project', projectID, steps)
                 return projectID
             }
         }
--- imports/ui/pages/userInfo/userInfo.js
+++ imports/ui/pages/userInfo/userInfo.js
@@ -2,12 +2,110 @@ import './userInfo.html'
 
 import { Template } from 'meteor/templating'
 import { UserQuestions } from '/imports/api/userQuestions/userQuestions'
+import { FormProgress } from '/imports/api/form-progress/form-progress'
+import { addUserInfo } from '/imports/api/userQuestions/methods'
 
 import { AutoForm } from 'meteor/aldeed:autoform'
 import { notify } from '/imports/modules/notifier'
 
 Template.userInfo.onCreated(function() {
 	window.UserQuestions = UserQuestions
+	this.userInfoID = FlowRouter.getParam("userInfoID")
+
+	this.autorun(() => {
+		this.subscribe('userQuestions', FlowRouter.getParam("userInfoID"))
+		this.subscribe('formProgress', FlowRouter.getParam("userInfoID"))
+	})
+})
+
+Template.userInfo.onRendered(function() {
+	this.autorun(() => {
+		const wizard = Wizard.get('user-info-wizard');
+		this.wizard = wizard;
+
+		const progress = FormProgress.findOne({ 'form_type_id' : FlowRouter.getParam("userInfoID")  })
+
+		if (progress !== undefined) {
+			const userInfo = UserQuestions.findOne({ '_id' : FlowRouter.getParam("userInfoID") })
+			
+			for (let i = 0; i < wizard.steps.length; i++) {
+				const step = wizard.steps[i];
+				if (step.id === progress.next_step) break;
+
+				const stepData = {}
+				const schemaKeys = step.schema._schemaKeys
+
+				for (let j = 0; j < schemaKeys.length; j++) {
+					const schemaKey = schemaKeys[j];
+					stepData[schemaKey] = userInfo[schemaKey]
+				}
+
+				wizard.setData(step.id, stepData)
+			}
+
+			wizard.show(progress.next_step)
+		}
+		
+	})
+})
+
+Template.userInfo.helpers({
+	steps() {
+		return [{
+			id: 'stepOne',
+			title: 'Step One',
+			formId: 'stepOne', 
+			schema: UserQuestions.schema.pick(
+				'fullName',
+				'dob',
+				'country',
+				'github',
+				'reason'
+			),
+		}, {
+			id: 'stepTwo',
+			title: 'Step Two',
+			formId: 'stepTwo', 
+			schema: UserQuestions.schema.pick(
+				'teamDescription',
+				'averageTeamWorkday',
+				'timeCommitment',
+				'personalInvestment'
+			),
+		}, {
+			id: 'stepThree',
+			title: 'Step Three',
+			formId: 'stepThree', 
+			schema: UserQuestions.schema.pick(
+				'relatedPastProjects',
+				'projectGoals',
+				'likedBlockchainProjects',
+				'employmentStatus'
+			),
+		}]
+	}
+})
+
+Template.userInfo.events({
+	'submit' (event, tpl) {
+		event.preventDefault();
+
+		let activeStep = tpl.wizard.activeStep()
+		let userInfoID = tpl.userInfoID === undefined ? 'new' : tpl.userInfoID
+
+		let steps = {}
+		let nextStep = activeStep.id
+		steps.last = activeStep.wizard._stepsByIndex[activeStep.wizard.indexOf(activeStep.id)-1]
+		steps.next = nextStep
+
+		addUserInfo.call({ userInfoID: userInfoID, userInfo: activeStep.wizard.mergedData(), steps: steps}, (err, resp) => {
+			if (!err) {
+				if (userInfoID !== resp) { FlowRouter.setParams({userInfoID: resp}) };
+				activeStep.wizard.next();
+			}
+		})
+		
+	}
 })
 
 AutoForm.addHooks(['userQuestionsForm'], {
