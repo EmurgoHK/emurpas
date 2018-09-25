e573fa691115e686990572543d1a7048d295995a
Samuel Ralak <samuelralak@hotmail.com>
2018-09-25 17:06:32 +0300

    problem: moderators cannot view applications #73
    solution: mod can view form progress details. clicking on view redirects to the single application page

--- imports/ui/pages/moderator/applications/modApplications.js
+++ imports/ui/pages/moderator/applications/modApplications.js
@@ -6,6 +6,7 @@ import { FormProgress } from '/imports/api/form-progress/form-progress'
 import { removeProjectQuestions } from '/imports/api/project-questions/methods'
 
 import swal from 'sweetalert'
+import moment from 'moment'
 import { notify } from '/imports/modules/notifier'
 
 Template.modApplications.onCreated(function() {
@@ -21,12 +22,46 @@ Template.modApplications.helpers({
             createdAt: -1
         }
     }),
-    isInProgress: function() {
-        let inprogress = FormProgress.findOne({
-            form_type_id: this._id
+    isInProgress: function(status) {
+        return status == 'in-progress'
+    },
+    formProgress: function () {
+        var progress = FormProgress.find({
+            form_type: 'project'
+        }, {
+            sort: {
+                createdAt: -1
+            }
         })
-        
-        return inprogress && inprogress.status == 'in-progress'
+
+        return progress
+    },
+    author: (userId) => {
+        let user = Meteor.users.findOne({ _id: userId })
+        return user.emails[0].address
+    },
+    formatDate: (timestamp) => {
+        return moment(timestamp).format('MMMM Do YYYY, h:mm a')
+    },
+    formatProgressStatus: (status, progressID) => {
+        let formatted = {}
+        formatted.status = status
+        formatted.id = progressID
+
+        if (status === 'completed') {
+            formatted.klass = 'success'
+            formatted.text = 'View'
+            formatted.path = 'view'
+
+        }
+            
+        if (status === 'in-progress') {
+            formatted.klass = 'secondary'
+            formatted.text = 'Resume'
+            formatted.path = ''
+        }
+
+        return formatted
     }
 })
 
@@ -53,7 +88,7 @@ Template.modApplications.events({
         }).then(confirmed => {
             if (confirmed) {
                 removeProjectQuestions.call({
-                    projectId: this._id
+                    projectId: this.form_type_id
                 }, (err, data) => {
                     if (err) {
                         notify(err.reason || err.message, 'error')
