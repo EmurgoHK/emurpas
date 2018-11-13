a92cc77b60ca7fee7fe825381b2ab79d0b13c9ac
Madan Bhandari <madan.kbh@gmail.com>
2018-11-13 10:39:06 +0530

    Problem: hamburger menu doesn't work #138
    Solution : Added check for opened sidebar on starting before toggle sidebar-open

--- imports/ui/shared/header/header.js
+++ imports/ui/shared/header/header.js
@@ -4,36 +4,39 @@ import './header.scss'
 import { Notifications } from '/imports/api/notifications/notifications'
 
 Template.header.onCreated(function() {
-    this.autorun(() => {
-        this.subscribe('notifications')
-    })
+  this.autorun(() => {
+    this.subscribe('notifications')
+  })
 })
 
 Template.header.events({
-    'click .sidebar-toggler' (event) {
-        event.preventDefault()
-        $('body').toggleClass("sidebar-show")
-    },
-
-    'click #signOut' (event) {
-        event.preventDefault()
-
-        if (Meteor.userId()) { 
-            Meteor.logout() 
-        }
+  'click .sidebar-toggler' (event) {
+    event.preventDefault()
+    if($('body').hasClass('sidebar-lg-show')) {
+      $('body').removeClass('sidebar-lg-show')
+    } else {
+      $('body').toggleClass('sidebar-show')
     }
+  },
+
+  'click #signOut' (event) {
+    event.preventDefault()
+    if (Meteor.userId()) { 
+      Meteor.logout() 
+    }
+  }
 })
 
 Template.header.helpers({
-    notificationsCount: () => Notifications.find({
-        userId: Meteor.userId(),
-        read: false,
-        $or: [{
-            type: 'notification'
-        }, {
-            type: {
-                $exists: false
-            }
-        }]
-    }).count()
+  notificationsCount: () => Notifications.find({
+    userId: Meteor.userId(),
+    read: false,
+    $or: [{
+      type: 'notification'
+    }, {
+      type: {
+        $exists: false
+      }
+    }]
+  }).count()
 })
