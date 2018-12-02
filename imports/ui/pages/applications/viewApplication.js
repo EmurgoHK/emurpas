2b6fc53e389d47c6fb03bbd02534940be6b13d17
Dilpreet Singh <singhsince94@gmail.com>
2018-12-03 00:25:19 +0530

    problem: no application status description #132
    solution: Adds status for the application on the user application route.
    
    Added status:
    - Incomplete application form
    - Waiting for team members to complete personal information
    - Your application is being reviewed
    - The reviewers have questions for you, please view your application to respond
    - Rejected

--- imports/ui/pages/applications/viewApplication.js
+++ imports/ui/pages/applications/viewApplication.js
@@ -7,6 +7,8 @@ import { newComment } from '/imports/api/comments/methods'
 
 import { ProjectQuestions } from '/imports/api/project-questions/project-questions';
 import { QuestionRating } from '/imports/api/question-rating/question-rating'
+import { UserQuestions } from '/imports/api/userQuestions/userQuestions'
+import { FormProgress } from '/imports/api/form-progress/form-progress'
 
 import { notify } from '/imports/modules/notifier'
 
@@ -15,8 +17,10 @@ Template.viewApplication.onCreated(function() {
         this.subscribe('projectQuestions', FlowRouter.getParam('projectID'))
         this.subscribe('formProgress', FlowRouter.getParam('projectID'))
         this.subscribe('questionRating.all')
+    	this.subscribe('formProgress')
 
         this.subscribe('users')
+        this.subscribe('userQuestions')
         this.subscribe('comments.item', FlowRouter.getParam('projectID'))
     })
 
@@ -126,5 +130,71 @@ Template.viewApplication.helpers({
             resourceId: FlowRouter.getParam('projectID')
         }).count()
     },
-    type: () => 'comment'
+    type: () => 'comment',
+    applicationStatus: () => {
+        let application = ProjectQuestions.findOne({
+            _id: FlowRouter.getParam('projectID')
+        }) || {}
+
+        if(application && application.team_members) {
+            
+            let teamComplete = true
+            let hasModQuestions = false
+            
+
+            // check if the user completed the form
+            let formProgress = FormProgress.findOne({
+              form_type_id: application._id
+            })
+
+            let isFormComplete = formProgress && formProgress.status == 'completed'
+
+            // check if all the users in the team have filled the user questions
+            application.team_members.forEach(function(member) {
+                
+                // get the team member by email
+                let user = Meteor.users.findOne({ emails: {
+                    $elemMatch:  {
+                        address : member.email
+                    }
+                }})
+
+                let userInfo =[]
+                // check if the user has field the user questions
+                if(user) {
+                    userInfo = Array.from(UserQuestions.find({
+                        createdBy: user._id
+                    }))
+                }
+                
+                // if one of the member has not completed the user questions then the form is incomplete
+                if(userInfo.length < 1) teamComplete = false
+            })
+
+            // check if there is a question from moderators
+            let questions = Array.from(Comments.find({
+                parentId: FlowRouter.getParam('projectID'),
+                type: "question"
+            }))
+
+            // if there are moderator questions the we need to change the status of modQuestions flag
+            if (questions.length > 0) hasModQuestions = true
+
+            // set status for UI on page
+            if (!isFormComplete) {
+                return { message: 'Incomplete application form', class: 'secondary'}
+            }
+            else if (!teamComplete) {
+                return { message: 'Waiting for team members to complete personal information', class: 'secondary'}
+            } else if(hasModQuestions) {
+                return { message: 'The reviewers have questions for you, please view your application to respond', class: 'warning'}
+            } else if (application.status && application.status == 'rejected') {
+                return { message: 'This application was rejected', class: 'danger'}
+            } else {
+                return { message: 'Your application is being reviewed', class: 'primary'}
+            }
+        }
+
+        
+    }
 })
