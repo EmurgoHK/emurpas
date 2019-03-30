cf69ebceef56a0396d7db43f4fc2e71e1b9ee782
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2019-03-30 20:15:43 +0100

    Problem: registration doesn't work locally #238
    Solution: made onCreateUser more resilient

--- imports/api/user/server/accounts.js
+++ imports/api/user/server/accounts.js
@@ -1,20 +1,17 @@
-import { Meteor } from 'meteor/meteor'
-import SimpleSchema from 'simpl-schema'
-
 import { ProjectQuestions } from '/imports/api/project-questions/project-questions'
 
 Accounts.onCreateUser((options, user) => {
 	user.profile = options.profile || {}
 
-  	let projects = _.flatten(ProjectQuestions.find({}).fetch().map(i => {
-  		return (i.team_members || []).map(j => ({
-  			name: j.name,
-  			address: j.email,
-  			applicationId: i._id
-  		}))
-  	}))
+	let projects = _.flatten(ProjectQuestions.find({}).fetch().map(i => {
+		return ((i.team_members && i.team_members instanceof Array && i.team_members) || []).map(j => ({
+			name: j.name,
+			address: j.email,
+			applicationId: i._id
+		}))
+	}))
 
-  	let project = projects.filter(i => i.address === ((user.emails || [])[0] || {}).address)
+	let project = projects.filter(i => i.address === ((user.emails || [])[0] || {}).address)
 
 	if (project.length > 0) {
 		user.profile.team = {
