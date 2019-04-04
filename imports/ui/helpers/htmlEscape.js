0e2c85f3b4077fd0394dac122d82ed2c6d2e0053
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2019-04-04 18:21:03 +0200

    Problem: XSS vulnerability found #250
    Solution:
    - escaping labels before display
    - moved to using input: 'radio'

---
+++ imports/ui/helpers/htmlEscape.js
@@ -0,0 +1,12 @@
+// https://stackoverflow.com/questions/1787322/htmlspecialchars-equivalent-in-javascript/4835406#4835406
+export function escapeHtml(text) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
