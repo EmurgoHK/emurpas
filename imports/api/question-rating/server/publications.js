865377c36db9fee136b07eadaa0998aec1181609
Mihaly Lengyel <mihaly.lengyel@tresorit.com>
2019-04-04 13:02:19 +0200

    Problem: broad publications security issue #243
    Solution: reviewed and tightened publications

--- imports/api/question-rating/server/publications.js
+++ imports/api/question-rating/server/publications.js
@@ -1,4 +1,34 @@
-import { Meteor } from 'meteor/meteor'
-import { QuestionRating } from '../question-rating'
+import { Meteor } from "meteor/meteor";
+import { QuestionRating } from "../question-rating";
+import { isModerator } from "../../user/methods";
+import { ProjectQuestions } from "../../project-questions/project-questions";
 
-Meteor.publish('questionRating.all', () => QuestionRating.find({}))
+Meteor.publish("questionRating.application", appId => {
+  const userId = Meteor.userId();
+  if (!userId) {
+    throw new Meteor.Error("Error", "You have to be logged in.");
+  }
+
+  if (
+    !isModerator(userId) &&
+    !ProjectQuestions.findOne({
+      _id: appId,
+      $or: [
+        {
+          createdBy: userId
+        },
+        {
+          "team_members.email": ((Meteor.user().emails || [])[0] || {}).address
+        }
+      ]
+    })
+  ) {
+    throw new Meteor.Error(
+      "Error",
+      "Resource not found or insufficient permissions."
+    );
+  }
+  return QuestionRating.find({
+    applications: appId
+  });
+});
--- imports/ui/pages/contact/contact.js
+++ imports/ui/pages/contact/contact.js
@@ -1,126 +1,137 @@
-import './contact.html'
+import "./contact.html";
 
-import { FlowRouter } from 'meteor/kadira:flow-router'
-import { Contact } from '/imports/api/contact/contact'
-import { Comments } from '/imports/api/comments/comments'
+import { FlowRouter } from "meteor/kadira:flow-router";
+import { Contact } from "/imports/api/contact/contact";
 
-import { removeContact } from '/imports/api/contact/methods'
+import { removeContact } from "/imports/api/contact/methods";
 
-import { notify } from '/imports/modules/notifier'
-import moment from 'moment'
+import { notify } from "/imports/modules/notifier";
+import moment from "moment";
 
 // export this one so it can be used on other templates
 export const formatStatus = status => {
-    let formatted = {}
-    formatted.status = status
+  let formatted = {};
+  formatted.status = status;
 
-    if (status === 'resolved') {
-        formatted.klass = 'success'
-    }
-            
-    if (status === 'open') {
-        formatted.klass = 'primary'
-    }
+  if (status === "resolved") {
+    formatted.klass = "success";
+  }
 
-    if (status === 'closed') {
-        formatted.klass = 'danger'
-    }
+  if (status === "open") {
+    formatted.klass = "primary";
+  }
 
-    return formatted
-}
+  if (status === "closed") {
+    formatted.klass = "danger";
+  }
 
+  return formatted;
+};
+const CommentCounts = new Mongo.Collection("commentCounts");
 Template.contact.onCreated(function() {
-  	this.autorun(() => {
-    	this.subscribe('contact')
-        this.subscribe('comments')
-    	this.subscribe('users')
-  	})
+  this.subscribe("contact");
+  this.subscribe("commentCounts.by.resource");
+  this.subscribe("users");
 
-    this.filter = new ReactiveVar(['open'])
-})
+  this.filter = new ReactiveVar(["open"]);
+});
 
 Template.contact.helpers({
-    checked: function() {
-        return ~Template.instance().filter.get().indexOf(this.choice)
+  checked: function() {
+    return ~Template.instance()
+      .filter.get()
+      .indexOf(this.choice);
+  },
+  statuses: () => [
+    {
+      choice: "open",
+      text: "Open"
     },
-    statuses: () => [{
-        choice: 'open',
-        text: 'Open'
-    }, {
-        choice: 'closed',
-        text: 'Closed'
-    }, {
-        choice: 'resolved',
-        text: 'Resolved'
-    }],
-    hasContacts: () => Contact.find({}).count(),
-    contact: () => {
-    	let user = Meteor.users.findOne({
-    		_id: Meteor.userId()
-    	})
-
-        if (user) {
-            return Contact.find({
-                status: {
-                    $in: Template.instance().filter.get()
-                }
-            }, {
-                sort: {
-                    createdAt: -1
-                }
-            })
-        }
-
-        return false
+    {
+      choice: "closed",
+      text: "Closed"
     },
-    formatStatus: (status) => {
-        return formatStatus(status)
-    },
-    author: (userId) => {
-        let user = Meteor.users.findOne({ _id: userId }) || {}
-        return user.username ? user.username : ((user.emails || [])[0] || {}).address 
-    },
-    formatDate: (timestamp) => {
-        return moment(timestamp).format('MMMM Do YYYY, h:mm a')
-    },
-    commentCount: function() {
-        return Comments.find({
-            resourceId: this._id
-        }).count()
-    },
-    isAuthor: function() {
-        return this.createdBy === Meteor.userId()
+    {
+      choice: "resolved",
+      text: "Resolved"
     }
-})
+  ],
+  hasContacts: () => Contact.find({}).count(),
+  contact: () => {
+    let user = Meteor.users.findOne({
+      _id: Meteor.userId()
+    });
+
+    if (user) {
+      return Contact.find(
+        {
+          status: {
+            $in: Template.instance().filter.get()
+          }
+        },
+        {
+          sort: {
+            createdAt: -1
+          }
+        }
+      );
+    }
+
+    return false;
+  },
+  formatStatus: status => {
+    return formatStatus(status);
+  },
+  author: userId => {
+    let user = Meteor.users.findOne({ _id: userId }) || {};
+    return user.username
+      ? user.username
+      : ((user.emails || [])[0] || {}).address;
+  },
+  formatDate: timestamp => {
+    return moment(timestamp).format("MMMM Do YYYY, h:mm a");
+  },
+  commentCount: function() {
+    return (CommentCounts.findOne({ resourceId: this._id }) || {}).count || 0;
+  },
+  isAuthor: function() {
+    return this.createdBy === Meteor.userId();
+  }
+});
 
 Template.contact.events({
-    'click .remove': function(event, templateInstance) {
-        event.preventDefault()
+  "click .remove": function(event, templateInstance) {
+    event.preventDefault();
 
-        removeContact.call({
-            contactId: this._id
-        }, (err, data) => {
-            if (!err) {
-                notify('Successfully removed.', 'success')
-            } else {
-                notify(err.reason || err.message, 'error')
-            }
-        })
-    },
-    'click .filter-choice': function(event, templateInstance) {
-        if (!$(event.currentTarget).is(':checked')) {
-            // if it's not checked, remove it from the filter
-            templateInstance.filter.set(templateInstance.filter.get().filter(i => i !== this.choice))
+    removeContact.call(
+      {
+        contactId: this._id
+      },
+      (err, data) => {
+        if (!err) {
+          notify("Successfully removed.", "success");
         } else {
-            // else, if it's checked, add it to the filter
-            let filter = templateInstance.filter.get()
-
-            // just in case, check if it's already in the filter
-            if (!~filter.indexOf(this.choice)) {
-                filter.push(this.choice)
-            }
-
-            templateInstance.filter.set(filter)
+          notify(err.reason || err.message, "error");
         }
+      }
+    );
+  },
+  "click .filter-choice": function(event, templateInstance) {
+    if (!$(event.currentTarget).is(":checked")) {
+      // if it's not checked, remove it from the filter
+      templateInstance.filter.set(
+        templateInstance.filter.get().filter(i => i !== this.choice)
+      );
+    } else {
+      // else, if it's checked, add it to the filter
+      let filter = templateInstance.filter.get();
+
+      // just in case, check if it's already in the filter
+      if (!~filter.indexOf(this.choice)) {
+        filter.push(this.choice);
+      }
+
+      templateInstance.filter.set(filter);
     }
-})
+  }
+});
